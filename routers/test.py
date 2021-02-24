f0e1c53724ace901caa6f1307515f0874556a7a4
Thomas Hagelien <thomas.f.hagelien@sintef.no>
2021-02-24 15:10:11 +0100

    Added the broker and celery-service

--- routers/test.py
+++ routers/test.py
@@ -8,7 +8,7 @@
 
 from celery import Celery
 from subprocess import run
-app = Celery('tasks', broker='amqp://guest:guest@some-rabbit:5672//')
+app = Celery('tasks', broker='amqp://guest:guest@broker:5672//')
 @app.task
 def command(text):
     ret = run (["ls", "-al"])
