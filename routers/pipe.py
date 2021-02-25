5333d2ead7c8f402f899a7c0d9444c582e4325ef
Treesa <treesarjc@gmail.com>
2021-02-25 10:55:58 +0100

    pipe api structure

--- routers/pipe.py
+++ routers/pipe.py
@@ -6,16 +6,16 @@ router = APIRouter()
 async def create_pipe():
     return ' '
 
-@router.get("/")
-async def show_pipe():
-    return ' '
 
-@router.delete("/{pipe_id}") 
-async def delete_pipe():
-    return ' '
-
+# return data
 @router.get("/{pipe_id}")
-async def info_pipe():
+async def data_pipe():
+    return ' '
+
+# Add a mapper or datasource
+@router.post("/{pipe_id}/add")
+async def addInput_pipe():
+    # map mapper or datasource id
     return ' '
 
 #get data - no need /data
@@ -23,6 +23,14 @@ async def info_pipe():
 # async def info_mapping():
 #     return ' '
 
+@router.get("/")
+async def show_pipe():
+    return ' '
+
 @router.put("/{pipe_id}/flush")
 async def read_pipe():
+    return ' '
+
+@router.delete("/{pipe_id}") 
+async def delete_pipe():
     return ' '
--- routers/transformation.py
+++ routers/transformation.py
@@ -1,22 +1,14 @@
-from fastapi import APIRouter
-from fastapi import FastAPI, Depends, BackgroundTasks
+from fastapi import APIRouter, Depends
+from fastapi import FastAPI, Depends
 from abc import ABC, abstractmethod
 from typing import Optional, Dict
-from urllib.parse import urlparse
-from soft.dataspace.dataspace import Dataspace
 from pydantic import BaseModel
 from uuid import uuid4
 from ontotrans.transformation import TransformationContext
-from routers import test
 import json
 import fastapi_plugins
 import aioredis
-import pandas as pd
-import requests
-import pysftp
-import subprocess
-import os
-from tasks import command
+
 
 router = APIRouter()
 
@@ -25,31 +17,47 @@ class TransformationConfig(BaseModel):
     applicationName: str
     applicationType: str
 
-# run endpoint
-# addon for pipeid
 
-
+# Instantiate a transformation
 @router.post("/")
-async def create_transformation(config: TransformationConfig,
-                                cache: aioredis.Redis = Depends(
-                                    fastapi_plugins.depends_redis),
-                                ) -> Dict:
+async def create_transformation(
+    config: TransformationConfig,
+    cache: aioredis.Redis = Depends(fastapi_plugins.depends_redis),
+) -> Dict:
     transformation_id = str(uuid4())
     transformation_info = {
         'application_name': config.applicationName,
         'application_type': config.applicationType
     }
-    # to run
     await cache.set(transformation_id, json.dumps(transformation_info).encode('utf-8'))
     return dict(transformation_id=transformation_id)
 
-# connecting to a trans--starts and returns id
-# new transformation --id, run on id --new id for specfic run
 
-
-@router.get("/")
-async def show_transformation():
+# Add a pipe to get data/ info
+@router.post("/{transformation_id}/add")
+async def addPipe_transformation():
+    # map pipe id
     return ' '
+
+#Run a transformation
+@router.post("/{transformation_id}/run")
+async def run_transformation(
+    transformation_id: str,
+    cache: aioredis.Redis = Depends(fastapi_plugins.depends_redis),
+) -> Dict:
+    transformation_info = json.loads(await cache.get(transformation_id))
+    transformationctx = TransformationContext(
+        transformation_info['application_name'], transformation_info['application_type'])
+    transformationctx.write()
+    return 'Transformation complete'
+
+# returns transformation details
+@ router.get("/{transformation_id}/ref")
+async def info_transformation(
+    transformation_id: str,
+    cache: aioredis.Redis = Depends(fastapi_plugins.depends_redis),
+) -> Dict:
+    return json.loads(await cache.get(transformation_id))
 
 
 @router.get("/{transformation_id}")
@@ -57,32 +65,10 @@ async def status_transformation():
     # return someoutput
     return ' '
 
-
-@router.post("/{transformation_id}/add")
-async def addPipe_transformation():
-    # return someoutput
+@router.get("/")
+async def show_transformation():
     return ' '
-
-
-@router.post("/{transformation_id}/run")
-async def run_transformation(
-    transformation_id: str,
-    cache: aioredis.Redis = Depends(fastapi_plugins.depends_redis),
-    ) -> Dict:
-    transformation_info=json.loads(await cache.get(transformation_id))
-    transformationctx = TransformationContext(transformation_info['application_name'], transformation_info['application_type'])
-    transformationctx.write()
-    return 'Transformation complete '
-
 
 @ router.delete("/{transformation_id}")
 async def delete_transformation():
     return ' '
-
-
-@ router.get("/{transformation_id}/ref")
-async def info_transformation(
-    transformation_id: str,
-    cache: aioredis.Redis = Depends(fastapi_plugins.depends_redis),
-) -> Dict:
-    return json.loads(await cache.get(transformation_id))
