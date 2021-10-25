39648ac1a85de2654418f8ec5155347d520d7559
Treesa Joseph <treesa.joseph@sintef.no>
2021-10-25 15:06:39 +0200

    add plugins for compevo

--- plugins/filter-strategy/compevo-crop-filter.py
+++ plugins/filter-strategy/compevo-crop-filter.py
@@ -1,6 +1,6 @@
 # pylint: disable=W0511, W0613
 """
-Demo-filter strategy
+Filter plugin for compevo usecase (cropping the image)
 """
 from typing import Dict, Optional, Any
 from pydantic import BaseModel
@@ -9,13 +9,15 @@ from app.models.filterconfig import FilterConfig
 from app.strategy.factory import StrategyFactory
 import dlite
 
+
 class CropDataModel(BaseModel):
     crop: str
 
 
 @dataclass
 @StrategyFactory.register(
-    ('filterType', 'filter/compevo-crop') # No other plugin should have filter/compevo-crop!
+    # No other plugin should have filter/compevo-crop!
+    ('filterType', 'filter/compevo-crop')
 )
 class CompevoCropFilter:
 
@@ -24,18 +26,13 @@ class CompevoCropFilter:
     def initialize(self, session: Optional[Dict[str, Any]] = None) -> Dict:
         """ Initialize strategy and return a dictionary """
         cropData = CropDataModel(**self.filter_config.configuration)
-        return dict( crop=cropData.crop)
+        return dict(crop=cropData.crop)
 
     def get(self, session: Optional[Dict[str, Any]] = None) -> Dict:
-        """ Execute strategy and return a dictionary """
-
-               
-        coll = dlite.get_collection(session['collection_id']) #pylint: disable=W0612
-        
+        """ Crop the image """
+        coll = dlite.get_collection(session['collection_id'])
         image = coll.get('pore_image')
-
         coll.remove('pore_image')
-
         r = coll.find_first(s='pore_image', p='crop')
         x1, x2, y1, y2 = [int(i) for i in r.o.split(',')]
         data = image.data[y1:y2, x1:x2]
@@ -43,4 +40,4 @@ class CompevoCropFilter:
         cropped.data = data
         cropped.scale = image.scale
         coll.add('pore_image', cropped)
-        return dict(filetrstep='filterstep')
+        return dict(FilterCropStep='compevo-crop')
