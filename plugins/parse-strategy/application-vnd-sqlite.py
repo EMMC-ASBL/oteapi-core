8d002a2ce60acf7bd0084836a34314d0e4694ea7
Bjørn Tore Løvfall <blov@sintef.no>
2021-11-11 14:25:04 +0100

    Added sqlite calls to parse strategy
    (work in progress)

--- plugins/parse-strategy/application-vnd-sqlite.py
+++ plugins/parse-strategy/application-vnd-sqlite.py
@@ -5,13 +5,29 @@ from typing import Any, Dict, Optional
 
 from app.models.resourceconfig import ResourceConfig
 from app.strategy.factory import StrategyFactory
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
 
 
 @dataclass
 @StrategyFactory.register(
-    ("mediaType", "application/vnd.sqlite3"),
-    ("mediaType", "application/vnd.sqlite"),
-    ("mediaType", "application/sqlite")
+    ("mediaType", "application/vnd.sqlite3")
 )
 class SqliteParseStrategy:
 
@@ -21,11 +37,28 @@ class SqliteParseStrategy:
         self, session: Optional[Dict[str, Any]] = None  # pylint: disable=W0613
     ) -> Dict:
         print("Sqlite in action!")
-        return {}
+        print(session)
+        if (session == None):
+            raise HTTPException(
+                status_code=404,
+                detail="Missing session",)
+        if "sqlquery" in session:
+            print("querying")
+            cn = create_connection(session['filename'])
+            cur = cn.cursor()
+            rows = cur.execute(session["sqlquery"]).fetchall()
+            return dict(result=rows)
+        else:
+            return dict(result="No query given")
+        print(session)
+        print(self)
+        return {"STATUS" : "OK"}
 
     def initialize(
         self, session: Optional[Dict[str, Any]] = None  # pylint: disable=W0613
     ) -> Dict:
+        print(session)
+        self.res.update(session)
         """Initialize"""
         return dict()
 
