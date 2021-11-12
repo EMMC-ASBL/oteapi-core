0190e7e1a0de4ed4e943d3c16124e218d2f81ee0
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2021-11-12 16:16:46 +0100

    Use `pydantic.Field` for all config pydantic models

--- app/models/resourceconfig.py
+++ app/models/resourceconfig.py
@@ -4,21 +4,88 @@ Pydantic ResourceConfig Data Model
 
 from typing import Dict, Optional
 
-from pydantic import AnyUrl, BaseModel
+from pydantic import AnyUrl, BaseModel, Field
 
 
 class ResourceConfig(BaseModel):
     """Resource Configuration for Dataset Distributions"""
 
-    downloadUrl: AnyUrl = None
-    mediaType: str = None
-    accessUrl: AnyUrl = None
-    accessService: str = None
-    license: Optional[str]
-    accessRights: Optional[str]
-    description: Optional[str]
-    published: Optional[str]
-    configuration: Optional[Dict]
+    downloadUrl: Optional[AnyUrl] = Field(
+        None,
+        description=(
+            "Definition: The URL of the downloadable file in a given format. E.g. CSV "
+            "file or RDF file. The format is indicated by the distribution's "
+            "`dct:format` and/or `dcat:mediaType`.\n\nUsage: `dcat:downloadURL` "
+            "*SHOULD* be used for the URL at which this distribution is available "
+            "directly, typically through a HTTP Get request."
+        ),
+    )
+    mediaType: Optional[str] = Field(
+        None,
+        description=(
+            "The media type of the distribution as defined by IANA "
+            "[[IANA-MEDIA-TYPES](https://www.w3.org/TR/vocab-dcat-2/#bib-iana-media-types)]"
+            ".\n\nUsage: This property *SHOULD* be used when the media"
+            " type of the distribution is defined in IANA "
+            "[[IANA-MEDIA-TYPES](https://www.w3.org/TR/vocab-dcat-2/#bib-iana-media-types)]"
+            ", otherwise `dct:format` *MAY* be used with different values."
+        ),
+    )
+    accessUrl: Optional[AnyUrl] = Field(
+        None,
+        description=(
+            "A URL of the resource that gives access to a distribution of "
+            "the dataset. E.g. landing page, feed, SPARQL endpoint.\n\nUsage: "
+            "`dcat:accessURL` *SHOULD* be used for the URL of a service or location "
+            "that can provide access to this distribution, typically through a Web "
+            "form, query or API call.\n`dcat:downloadURL` is preferred for direct "
+            "links to downloadable resources.\nIf the distribution(s) are accessible "
+            "only through a landing page (i.e. direct download URLs are not known), "
+            "then the landing page URL associated with the `dcat:Dataset` *SHOULD* be "
+            "duplicated as access URL on a distribution (see "
+            "[§ 5.7 Dataset available only behind some Web page](https://www.w3.org/TR/vocab-dcat-2/#example-landing-page))."  # pylint: disable=line-too-long
+        ),
+    )
+    accessService: Optional[str] = Field(
+        None,
+        description=(
+            "A data service that gives access to the distribution of the "
+            "dataset.\n\nUsage: `dcat:accessService` *SHOULD* be used to link to a "
+            "description of a `dcat:DataService` that can provide access to this "
+            "distribution."
+        ),
+    )
+    license: Optional[str] = Field(
+        None,
+        description=(
+            "A legal document under which the distribution is made available.\n\nUsage:"
+            " Information about licenses and rights *SHOULD* be provided on the level "
+            "of Distribution. Information about licenses and rights *MAY* be provided "
+            "for a Dataset in addition to but not instead of the information provided "
+            "for the Distributions of that Dataset. Providing license or rights "
+            "information for a Dataset that is different from information provided for"
+            " a Distribution of that Dataset *SHOULD* be avoided as this can create "
+            "legal conflicts. See also guidance at "
+            "[§ 8. License and rights statements](https://www.w3.org/TR/vocab-dcat-2/#license-rights)."  # pylint: disable=line-too-long
+        ),
+    )
+    accessRights: Optional[str] = Field(
+        None,
+        description=(
+            "A rights statement that concerns how the distribution is "
+            "accessed.\n\nUsage: Information about licenses and rights *MAY* be "
+            "provided for the Distribution. See also guidance at "
+            "[§ 8. License and rights statements](https://www.w3.org/TR/vocab-dcat-2/#license-rights)."  # pylint: disable=line-too-long
+        ),
+    )
+    description: Optional[str] = Field(
+        None, description="A free-text account of the distribution."
+    )
+    published: Optional[str] = Field(None, description="")
+    configuration: Optional[Dict] = Field(
+        None,
+        description="Resource-specific configuration options given as key/value-pairs.",
+    )
 
 
 class OTEUrl(BaseModel):
--- app/models/transformationconfig.py
+++ app/models/transformationconfig.py
@@ -5,27 +5,48 @@ TransformationConfig data model definition
 from datetime import datetime
 from typing import Dict, List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class TransformationConfig(BaseModel):
     """Transformation data model"""
 
-    transformation_type: str
-    name: Optional[str]
-    description: Optional[str]
-    due: Optional[datetime]
-    priority: Optional[int]
-    secret: Optional[str]
-    configuration: Optional[Dict]
+    transformation_type: str = Field(
+        ...,
+        description="Type of registered transformation strategy. E.g., `dlite/transformation`.",
+    )
+    name: Optional[str] = Field(
+        None, description="Human-readable name of the transformation strategy."
+    )
+    description: Optional[str] = Field(
+        None, description="A free-text account of the transformation."
+    )
+    due: Optional[datetime] = Field(None, description="")
+    priority: Optional[int] = Field(None, description="")
+    secret: Optional[str] = Field(None, description="")
+    configuration: Optional[Dict] = Field(
+        None,
+        description="Transformation-specific configuration options given as key/value-pairs.",
+    )
 
 
 class TransformationStatus(BaseModel):
     """Return from transformation status"""
 
-    id: str
-    status: Optional[str]
-    messages: Optional[List[str]]
-    created: Optional[datetime]
-    startTime: Optional[datetime]
-    finishTime: Optional[datetime]
+    id: str = Field(..., description="ID for the given transformation process.")
+    status: Optional[str] = Field(
+        None, description="Status for the transformation process."
+    )
+    messages: Optional[List[str]] = Field(
+        None, description="Messages related to the transformation process."
+    )
+    created: Optional[datetime] = Field(
+        None,
+        description="Time of creation for the transformation process. Given in UTC.",
+    )
+    startTime: Optional[datetime] = Field(
+        None, description="Time when the transformation process started. Given in UTC."
+    )
+    finishTime: Optional[datetime] = Field(
+        None, description="Time when the tranformation process finished. Given in UTC."
+    )
