0c97d4646b5233625abad4b9963a1dd967a5d7fe
francescalb <francesca.l.bleken@sintef.no>
2021-11-15 22:46:52 +0100

    Added all plugins for dehydrogenation case
    There are 3 plugins:
    - plugins/parse-strategy/atomistic-structures.py
    Downloads and parses an xyz or poscar file from a downloadUrl.
    Parsing is done with ase and the result is store in a delite collection
    
    - plugins/transformation-strategy/calculate_molecule_energy.py
    Calculates molecule energy with ase.
    Takes a given molecule from a dlite colletion, transforms it back to an
    ase.Atoms object and calculates energy by means of EMT
    
    - plugins/transformation-strategy/calculate_reaction_energy.py
    Calculates reation energy. Takes a dictionary with reactant
    names and stoichiometric values as input. Negative stoichiometry for
    reactants and positive for products.
    
    This can be tested in the OteLib as follows:
    database = {"h2": "https://raw.githubusercontent.com/"
                      "susilehtola/erkale/master/tests/xyz/H2.xyz",
                "ethene": "https://raw.githubusercontent.com/nutjunkie"
                          "/IQmol/master/share/fragments/Molecules/Alkenes/Ethene.xyz",
                "ethane": "https://raw.githubusercontent.com/nutjunkie/"
                          "IQmol/master/share/fragments/Molecules/Alkanes/Ethane.xyz",
               }
    key="h2"
    downloadHydrogen = server.create_dataresource(
        downloadUrl=database[key],
        mediaType="chemical/x-xyz",
        configuration={"moleculeName":key})
    
    key = "ethane"
    downloadEthane = server.create_dataresource(
        downloadUrl=database[key],
        mediaType="chemical/x-xyz",
        configuration={"moleculeName":key})
    
    key = "ethene"
    downloadEthene = server.create_dataresource(
        downloadUrl=database[key],
        mediaType="chemical/x-xyz",
        configuration={"moleculeName":key})
    
    calcMoleculeTransformation1 =
        server.create_transformation(transformation_type="ase/calc-energy")
    calcMoleculeTransformation2 =
        server.create_transformation(transformation_type="ase/calc-energy")
    calcMoleculeTransformation3 =
        server.create_transformation(transformation_type="ase/calc-energy")
    
    reaction = {'Ethene':-1, 'H2': -1,'Ethane':3}
    calcReactionTransformation =
        server.create_transformation(transformation_type="reaction/calc-reaction-energy"
                                 configuration={"stoichiometricReactants":reaction})
    
    pipeline = downloadHydrogen >> calcMoleculeTransformation1 >>
    downloadEthene >> calcMoleculeTransformation2 >> downloadEthane >>
    calcMoleculeTransformation3 >> calcReactionTransformation
    pipeline.get()

--- plugins/parse-strategy/atomistic-structures.py
+++ plugins/parse-strategy/atomistic-structures.py
@@ -14,15 +14,15 @@ from app.strategy.factory import StrategyFactory
 @dataclass
 @StrategyFactory.register(
     ("mediaType", "chemical/x-xyz"),
-    ("mediaType", "chemical/x-cif"),
     ("mediaType", "chemical/x-vasp"),  # Not an official internet mediatype
 )
 class AtomisticStructureParseStrategy:
-
+    # Which filter to use for a parsing strategy?
+    # resource_config must be wrong, but is filter right? why?
     resource_config: ResourceConfig
 
     def __post_init__(self):
-        self.localpath = "/app/data"
+        self.localpath = "/ote-data"
         self.filename = self.resource_config.downloadUrl.path.rsplit("/", 1)[-1]
         if self.resource_config.configuration:
             self.conf = self.resource_config.configuration
@@ -30,44 +30,55 @@ class AtomisticStructureParseStrategy:
             self.conf = {}
 
     def initialize(
-        self, session: Optional[Dict[str, Any]] = None  # pylint: disable=W0613
+        self,
+        session: Optional[Dict[str, Any]] = None,
     ) -> Dict:
         """Initialize"""
-        coll = dlite.Collection()
-        dlite.get_collection(coll.uuid)
-        return dict(collection_id=coll.uuid)
+        # coll = dlite.Collection()
+        # dlite.get_collection(coll.uuid)
+        print("initializes parser", session)
+        return dict()  # collection_id=coll.uuid)
 
     def parse(
-        self, session: Optional[Dict[str, Any]] = None  # pylint: disable=W0613
+        self,
+        session: Optional[Dict[str, Any]] = None,
+        MoleculeModel: dlite.Instance = None,
     ) -> Dict:
+
+        # Read the atoms and create and ase.Atoms object
         atoms = ase.io.read(f"{self.localpath}/{self.filename}")
 
         # The Molecule.json contains metadata for energy which is not part of
         # the molecular structure files.
-        # dlite.storage_path.append(str('app/entities/*.json'))
-        print(os.getcwd())
-        print(os.listdir("/app/entities"))
-        print(dlite.storage_path)
-        for d in [dlite.storage_path[0]]:
-            print(os.listdir(d))
-        Molecule = dlite.Instance("json://Molecule.json")  # DLite Metadata
+        dlite.storage_path.append(str("/app/entities"))
+
+        if "collection_id" in session:
+
+            coll = dlite.get_collection(session["collection_id"])
+
+        else:
+            coll = dlite.Collection()
+            dlite.get_collection(coll.uuid)
+
+        if MoleculeModel == None:
+            MoleculeModel = dlite.Instance(  # Need to fix storagepath
+                "json:///app/entities/Molecule.json"
+            )  # DLite Metadata
 
         basename = os.path.splitext(f"{self.filename}")[0]
-        inst = Molecule(dims=[len(atoms), 3], id=basename)  # DLite instance
+
+        inst = MoleculeModel(dims=[len(atoms), 3], id=basename)  # DLite instance
         inst.symbols = atoms.get_chemical_symbols()
         inst.masses = atoms.get_masses()
         inst.positions = atoms.positions
+        print(coll)
 
-        inst.groundstate_energy = ""
+        inst.groundstate_energy = 0.0
 
-        # So we have read a molecule and linked it to the correct metadata (
-        # actually, we have made new instance of the metadata populated with
-        # values. We should probably just have the link between the Metadata
-        # and the file.
-        # How should we now make the collection?
-        coll = dlite.get_collection(session["collection_id"])
         coll.add(label=basename, inst=inst)
-
+        print(coll)
         # Return uuid of the collection that now includes the new parsed
         # molecule.
-        return dict(collection_id=coll.uuid)
+        # We are passing the data though, is that correct?
+
+        return dict(collection_id=coll.uuid, molecule_name=basename)
---
+++ plugins/transformation-strategy/calculate_molecule_energy.py
@@ -0,0 +1,76 @@
+# pylint: disable=W0613, W0511
+"""
+Transformation plugin for compevo usecase (fetch output and print it)
+"""
+
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+import dlite
+from ase import Atom, Atoms
+from ase.calculators.emt import EMT
+from pydantic import BaseModel
+
+from app.models.transformationconfig import TransformationConfig
+from app.strategy.factory import StrategyFactory
+
+
+class CalcMoleculeConfig(BaseModel):
+    moleculeName: Optional[str]
+
+
+@dataclass
+@StrategyFactory.register(("transformation_type", "ase/calc-energy"))
+class ASEMoleculeCalculation:
+    """Transformations"""
+
+    transformation_config: TransformationConfig
+
+    def __post_init__(self):
+
+        if self.transformation_config.configuration:
+            self.conf = self.transformation_config.configuration
+        else:
+            self.conf = {}
+
+    def initialize(
+        self,
+        session: Optional[Dict[str, Any]] = None,
+    ) -> Dict:
+        """Initialize a job"""
+        if "collection_id" in session:
+            coll = dlite.get_collection(session["collection_id"])
+            # print(session["collection_id"])
+        else:
+            coll = dlite.Collection()
+            dlite.get_collection(coll.uuid)
+        return dict(collection_id=coll.uuid)
+
+    def get(self, session: Optional[Dict[str, Any]] = None) -> Dict:
+        """Fetch result and print"""
+        coll = dlite.get_collection(session["collection_id"])
+        # Get the moleculeName from session if not defined
+        # Note that Session only has one molecule_name at any given time
+        # even if several molecules have been parsed
+        moleculeData = CalcMoleculeConfig(**self.conf)
+        if moleculeData.moleculeName is None:
+            moleculeData.moleculeName = session["molecule_name"]
+
+        # Get molecule from collection
+        molecule = coll.get(moleculeData.moleculeName)
+
+        # Make ase Atom object
+        ase_molecule = Atoms()
+        for i in range(0, molecule.natoms):
+            symbol = molecule.properties["symbols"][i]
+            mass = molecule.properties["masses"][i]
+            position = molecule.properties["positions"][i]
+            atom = Atom(symbol=symbol, mass=mass, position=position)
+            ase_molecule.append(atom)
+
+        # Calculate reaction energy and add value to molecule in collection
+        ase_molecule.calc = EMT()
+        molecule.groundstate_energy = ase_molecule.get_potential_energy()
+
+        # Store back into a dlite Molecule
+        return dict(collection_id=coll.uuid)
