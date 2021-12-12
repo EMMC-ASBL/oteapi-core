7da7478d44dfce1a3e9ab72371c7cf22d67f6050
Jesper Friis <jesper.friis@sintef.no>
2021-12-12 21:15:09 +0100

    Updated the excel parser, including the following changes: - added documentation for the configuration data model - `col_from` and `col_to` now accepts excel column letters in addition to   column numbers - added `headerRow` config option - changed the meaning of `header` for column selection using `headerRow` - added `newHeader` option for optional renaming of header columns - added convinient function `parse_recarray()` that returns the selected   region as a numpy array
    Question: the returned json object isn't very semantically described.
    Wouldn't it be better to return it as dlite instance?

--- plugins/parse_strategy/excel_xlsx.py
+++ plugins/parse_strategy/excel_xlsx.py
@@ -3,9 +3,10 @@
 import os
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 from openpyxl import load_workbook
+from openpyxl.utils import column_index_from_string
 from openpyxl.worksheet.worksheet import Worksheet
 from pydantic import BaseModel, DirectoryPath
 
@@ -14,34 +15,79 @@ from app.strategy.factory import StrategyFactory
 
 
 class XLSXParseDataModel(BaseModel):
+    """Data model for retrieving a rectangular section of an Excel sheet.
+
+    Fields:
+        worksheet: Name of worksheet to load.
+        row_from: Excel row number of first row.  Defaults to first
+          assigned row.
+        col_from: Excel column number or label of first column.
+          Defaults to first assigned column.
+        row_to: Excel row number of last row.  Defaults to last
+          assigned row.
+        col_to: Excel column number or label of last column.  Defaults
+          to last assigned column.
+        headerRow: Row number with the headers. Defaults to 1 if
+          header is given, otherwise None.
+        header: Optional list of column names, specifying the columns
+          to return.  These names they should match cells in `headerRow`.
+        newHeader: Optional list of new column names replacing `header`
+          in the output.
+        downloadDir: Directory where we expect to find the (possible
+          downloaded) excel file.  Defaults to the current directory,
+          unless the 'OTEAPI_downloadDir' environment variable is set.
+    """
+
     worksheet: str
     row_from: int = None
-    col_from: int = None
+    col_from: Union[int, str] = None
     row_to: int = None
-    col_to: int = None
-    header_positions: List = []
+    col_to: Union[int, str] = None
+    headerRow: int = None
+    header: List[str] = None
+    newHeader: List[str] = None
     downloadDir: DirectoryPath = (  # move to ResourceConfig??
         os.environ["OTEAPI_downloadDir"] if "OTEAPI_downloadDir" in os.environ else "."
     )
 
 
-def fetch_headers(model_object: XLSXParseDataModel, worksheet: Worksheet) -> List[str]:
-    """
-    Helper function returning the headers of the worksheet as a list of strings.
-    If the list of headers is empty we assume the first row contains the headers
-    """
-    if len(model_object.header_positions) == 0:
-        return [
-            worksheet.cell(worksheet.min_row, col).value
-            for col in range(worksheet.min_column, worksheet.max_column + 1)
-        ]
-    else:
-        uppercase_codes = []
-        for code in model_object.header_positions:
-            uppercase_codes.append(code.upper())
+def set_model_defaults(model: XLSXParseDataModel, worksheet: Worksheet):
+    """Update datamodel `model` with default values obtained from `worksheet`."""
+    if model.row_from is None:
+        if model.header:
+            # assume that data starts on the first row after the header
+            model.row_from = model.headerRow + 1
+        else:
+            model.row_from = worksheet.min_row
 
-        uppercase_codes.sort()
-        return [worksheet[code].value for code in uppercase_codes]
+    if model.row_to is None:
+        model.row_to = worksheet.max_row
+
+    if model.col_from is None:
+        model.col_from = worksheet.min_column
+    elif isinstance(model.col_from, str):
+        model.col_from = column_index_from_string(model.col_from)
+
+    if model.col_to is None:
+        model.col_to = worksheet.max_column
+    elif isinstance(model.col_to, str):
+        model.col_to = column_index_from_string(model.col_to)
+
+    if model.header and not model.headerRow:
+        model.headerRow = 1
+
+
+def get_column_indices(model: XLSXParseDataModel, worksheet: Worksheet) -> List[int]:
+    """Helper function returning a list of column indices."""
+    if model.header:
+        header_dict = {
+            worksheet.cell(model.headerRow, col).value: col
+            for col in range(model.col_from, model.col_to + 1)
+        }
+        indices = [header_dict[h] for h in model.header]
+    else:
+        indices = range(model.col_from, model.col_to + 1)
+    return indices
 
 
 @dataclass
@@ -65,50 +111,78 @@ class XLSXParseStrategy:
             self.config = {}
 
     def parse(self, session: Optional[Dict[str, Any]] = None) -> Dict:
-        xlsx_parse_data = XLSXParseDataModel(**self.config)
-        filename = Path(xlsx_parse_data.downloadDir) / self.path.name
+        """Parses selected region of an excel file.  The fields of the
+        returned dict are as follows:
+        - header: list of column headers (read from `headerRow`).  Is None
+            if neither `headerRow` nor `header` configuration is given.
+        - data: cell values (as a list of lists, row-wise)
+        """
+        model = XLSXParseDataModel(**self.config)
+        filename = Path(model.downloadDir) / self.path.name
         workbook = load_workbook(filename=filename, read_only=True, data_only=True)
-        worksheet = workbook[xlsx_parse_data.worksheet]
+        worksheet = workbook[model.worksheet]
+        set_model_defaults(model, worksheet)
+        columns = get_column_indices(model, worksheet)
 
-        headers = fetch_headers(xlsx_parse_data, worksheet)
-        if xlsx_parse_data.row_from is None:
-            xlsx_parse_data.row_from = (
-                worksheet.min_row + 1
-            )  # We assume first row is headers
-        if xlsx_parse_data.row_to is None:
-            xlsx_parse_data.row_to = worksheet.max_row
-        if xlsx_parse_data.col_from is None:
-            xlsx_parse_data.col_from = worksheet.min_column
-        if xlsx_parse_data.col_to is None:
-            xlsx_parse_data.col_to = worksheet.max_column
-        json_data = {}
+        data = []
         for row in worksheet.iter_rows(
-            min_row=xlsx_parse_data.row_from,
-            min_col=xlsx_parse_data.col_from,
-            max_row=xlsx_parse_data.row_to
-            if xlsx_parse_data.row_to
-            else worksheet.max_row,
-            max_col=xlsx_parse_data.col_to
-            if xlsx_parse_data.col_to
-            else worksheet.max_column,
+            min_row=model.row_from,
+            max_row=model.row_to,
+            min_col=min(columns),
+            max_col=max(columns),
         ):
+            data.append([row[c - 1].value for c in columns])
 
-            doc = {}
-            data = []
-            for cell in row:
-                data.append(cell.value)
+        if model.headerRow:
+            row = worksheet.iter_rows(
+                min_row=model.headerRow,
+                max_row=model.headerRow,
+                min_col=min(columns),
+                max_col=max(columns),
+            ).__next__()
+            header = [row[c - 1].value for c in columns]
+        else:
+            header = None
 
-            if data[0] == None or data[-1] == None:
-                continue
+        if model.newHeader:
+            nhead = len(header) if header else len(data[0]) if data else 0
+            if len(model.newHeader) != nhead:
+                raise TypeError(
+                    f"length of `newHeader` (={len(model.newHeader)}) "
+                    f"doesn't match number of columns (={len(header)})"
+                )
+            if header:
+                for i, val in enumerate(model.newHeader):
+                    if val is not None:
+                        header[i] = val
+            elif data:
+                header = model.newHeader
 
-            for idx in range(1 + xlsx_parse_data.col_to - xlsx_parse_data.col_from):
-                doc[headers[idx]] = data[idx]
+        return {"header": header, "data": data}
 
-            current_row = row[0].row
-            json_data["Row " + str(current_row)] = doc
+    def parse_recarray(self, session: Optional[Dict[str, Any]] = None) -> Dict:
+        """Convenient method that works like `parse()`, but returns the data
+        as a numpy recarray.
+        """
+        import numpy as np
 
-        return json_data
+        d = self.parse(session)
+        data = d["data"]
+        nhead = len(d["header"]) if d["header"] else 0
+        nrows = len(data)
+        ncols = len(data[0]) if nrows else nhead
 
-    def initialize(self, session: Optional[Dict[str, Any]] = None) -> Dict:
-        """Initialize"""
-        return {}
+        # Set blank cells in numerical columns to NaN such that
+        # np.rec.fromrecords() will convert them to the correct
+        # numerical dtype
+        for col in range(ncols):
+            if all(
+                isinstance(data[row][col], (bool, int, float, complex, None.__class__))
+                for row in range(nrows)
+            ):
+                for row in range(nrows):
+                    if data[row][col] is None:
+                        data[row][col] = np.nan
+
+        d["data"] = np.rec.fromrecords(data, names=d["header"])
+        return d
