0450dce7a18950bb45529ea4c23de36ffe863d88
Casper Welzel Andersen <casper.andersen@epfl.ch>
2022-01-13 17:46:28 +0100

    Add utility functionality for release
    New `tasks.py` to hold `invoke` tasks to administrate the repository.
    Move information to `oteapi.__init__.py` and away from `setup.py` -> one
    source of truth.

--- oteapi/__init__.py
+++ oteapi/__init__.py
@@ -1,3 +1,5 @@
 """OTE-API Core package."""
 
 __version__ = "0.0.1"
+__author__ = "TEAM 4.0 devs"
+__author_email__ = "Team4.0@SINTEF.onmicrosoft.com"
--- setup.py
+++ setup.py
@@ -1,9 +1,43 @@
 """Setup for OTE-API."""
 from pathlib import Path
+import re
 
 from setuptools import find_packages, setup
 
 TOP_DIR = Path(__file__).resolve().parent
+PACKAGE_NAME = "oteapi-core"
+
+with open(
+    TOP_DIR / PACKAGE_NAME.replace("-", "_") / "__init__.py", "r", encoding="utf8"
+) as handle:
+    VERSION = AUTHOR = AUTHOR_EMAIL = None
+    for line in handle.readlines():
+        VERSION_match = re.match(r'__version__ = (\'|")(?P<version>.+)(\'|")', line)
+        AUTHOR_match = re.match(r'__author__ = (\'|")(?P<author>.+)(\'|")', line)
+        AUTHOR_EMAIL_match = re.match(
+            r'__author_email__ = (\'|")(?P<email>.+)(\'|")', line
+        )
+
+        if VERSION_match is not None:
+            VERSION = VERSION_match
+        if AUTHOR_match is not None:
+            AUTHOR = AUTHOR_match
+        if AUTHOR_EMAIL_match is not None:
+            AUTHOR_EMAIL = AUTHOR_EMAIL_match
+
+    for info, value in {
+        "version": VERSION,
+        "author": AUTHOR,
+        "author email": AUTHOR_EMAIL,
+    }.items():
+        if value is None:
+            raise RuntimeError(
+                f"Could not determine {info} from "
+                f"{TOP_DIR / PACKAGE_NAME.replace('-', '_') / '__init__.py'} !"
+            )
+    VERSION = VERSION.group("version")  # type: ignore[union-attr]
+    AUTHOR = AUTHOR.group("author")  # type: ignore[union-attr]
+    AUTHOR_EMAIL = AUTHOR_EMAIL.group("email")  # type: ignore[union-attr]
 
 BASE = [
     f"{_.strip()}"
@@ -12,12 +46,12 @@ BASE = [
 ]
 
 setup(
-    name="oteapi-core",
-    version="0.0.1",
-    author="TEAM 4.0 devs",
-    author_email="team4.0@SINTEF.onmicrosoft.com",
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
     url="https://github.com/EMMC-ASBL/oteapi-core",
-    description="OTE-API (Open Translation Environment) API.",
+    description="Open Translation Environment (OTE) API.",
     long_description=(TOP_DIR / "README.md").read_text(encoding="utf8"),
     long_description_content_type="text/markdown",
     packages=find_packages(),
