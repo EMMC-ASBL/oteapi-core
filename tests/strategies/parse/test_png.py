905dad3b3917f035890218d5e9b342398290cb51
Torgeir Ustad <Torgeir.Ustad@sintef.no>
2022-02-01 18:00:33 +0100

    Add test of PNG parse strategy

---
+++ tests/strategies/parse/test_png.py
@@ -0,0 +1,32 @@
+"""Tests the parse strategy for PNG."""
+# pylint: disable=unused-argument
+import os
+from pathlib import Path
+
+
+def test_png(import_oteapi_modules):
+    """Test the `image/png` parse strategy on 'sample_640_426.png',
+    downloaded from filesamples.com (called 'sample_640x426.png').
+    """
+    from oteapi.models.resourceconfig import ResourceConfig
+    from oteapi.strategies.parse.image import ImageDataParseStrategy
+
+    this_dir = Path(__file__).resolve().parent
+    config = ResourceConfig(
+        downloadUrl="file://dummy",
+        mediaType="image/png",
+        configuration={
+            "localpath": str(this_dir),
+            "filename": "sample_640_426.png",
+            "crop": (100, 50, 450, 300),
+        },
+    )
+    parser = ImageDataParseStrategy(config)
+    parser.parse()
+
+    with open(this_dir / "sample_350_250.png", "rb") as target:
+        target_data = target.read()
+    with open(this_dir / "cropped_sample_640_426.png", "rb") as cropped:
+        cropped_data = cropped.read()
+    os.remove(this_dir / "cropped_sample_640_426.png")
+    assert cropped_data == target_data
