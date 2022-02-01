400a149bd3ef9e0acda159ba0d71cbf96d7aa564
Torgeir Ustad <Torgeir.Ustad@sintef.no>
2022-02-01 17:50:17 +0100

    Add test of JPEG 2000 (JP2) parse strategy

---
+++ tests/strategies/parse/test_jp2.py
@@ -0,0 +1,32 @@
+"""Tests the parse strategy for JPEG 2000."""
+# pylint: disable=unused-argument
+import os
+from pathlib import Path
+
+
+def test_jp2(import_oteapi_modules):
+    """Test the `image/jp2` parse strategy on 'sample1_1000_1000.jp2,
+    a cropped subset of 'sample1.jp2' downloaded from filesamples.com.
+    """
+    from oteapi.models.resourceconfig import ResourceConfig
+    from oteapi.strategies.parse.image import ImageDataParseStrategy
+
+    this_dir = Path(__file__).resolve().parent
+    config = ResourceConfig(
+        downloadUrl="file://dummy",
+        mediaType="image/jp2",
+        configuration={
+            "localpath": str(this_dir),
+            "filename": "sample1.jp2",
+            "crop": (1000, 1000, 2000, 2000),
+        },
+    )
+    parser = ImageDataParseStrategy(config)
+    parser.parse()
+
+    with open(this_dir / "sample1_1000_1000.jp2", "rb") as target:
+        target_data = target.read()
+    with open(this_dir / "cropped_sample1.jp2", "rb") as cropped:
+        cropped_data = cropped.read()
+    os.remove(this_dir / "cropped_sample1.jp2")
+    assert cropped_data == target_data
