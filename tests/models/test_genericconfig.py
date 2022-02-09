02755c3acce7c145ae17366e8c5a17f9eaa76d37
Torgeir Ustad <Torgeir.Ustad@sintef.no>
2022-02-09 15:59:43 +0100

    Fixup test_genericconfig.py
    Use `@pytest.fixture` instead of a global object.

--- tests/models/test_genericconfig.py
+++ tests/models/test_genericconfig.py
@@ -1,19 +1,31 @@
 """Tests for `oteapi.models.genericconfig`"""
-from oteapi.models.genericconfig import GenericConfig
+from typing import TYPE_CHECKING
 
-generic_config = GenericConfig(
-    configuration={
-        "float": 3.14,
-        "integer": 5,
-        "string": "foo",
-    }
-)
+import pytest
+
+if TYPE_CHECKING:  # pragma: no cover
+    from oteapi.models.genericconfig import GenericConfig
+
+
+@pytest.fixture
+def generic_config() -> "GenericConfig":
+    """Return a usable `GenericConfig` for test purposes."""
+    from oteapi.models.genericconfig import GenericConfig
+
+    return GenericConfig(
+        configuration={
+            "float": 3.14,
+            "integer": 5,
+            "string": "foo",
+        }
+    )
 
 
 def test_subclass_description() -> None:
     """Test that the default description of the `configuration`
     attribute of a subclass of `GenericConfig` is the class docstring.
     """
+    from oteapi.models.genericconfig import GenericConfig
 
     class SomeSubclass(GenericConfig):
         """This is a subclass of GenericConfig."""
@@ -22,17 +34,19 @@ def test_subclass_description() -> None:
     assert instance.description == instance.__doc__
 
 
-def test_attribute_get_item() -> None:
+def test_attribute_get_item(generic_config: "GenericConfig") -> None:
     """Test configuration.__getitem__."""
     assert generic_config.configuration["integer"] == 5
 
 
-def test_attribute_set() -> None:
+def test_attribute_set(generic_config: "GenericConfig") -> None:
     """Test configuration.__setitem__, and thus also
     configuration.__setattr__.
+
+    Assign both a valid and an invalid value, to test dynamic
+    type-checking.
     """
     generic_config.configuration["string"] = "bar"
-    # Test dynamic type checking
     try:
         generic_config.configuration["string"] = 3.14
         assert False
@@ -40,12 +54,12 @@ def test_attribute_set() -> None:
         pass
 
 
-def test_attribute_contains() -> None:
+def test_attribute_contains(generic_config: "GenericConfig") -> None:
     """Test confguration.__contains__."""
     assert "float" in generic_config.configuration
 
 
-def test_attribute_del_item() -> None:
+def test_attribute_del_item(generic_config: "GenericConfig") -> None:
     """Test configuration.__delitem__."""
     del generic_config.configuration["float"]
     assert "float" not in generic_config.configuration
