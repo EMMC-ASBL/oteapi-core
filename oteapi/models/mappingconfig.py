63e961eee558d59c34373f9eaf8354fdf4926071
Jesper Friis <jesper-friis@users.noreply.github.com>
2022-03-10 18:25:06 +0100

    Added mapping strategy (#112)
    * Added mapping strategy
    
    * Suggested changes from Casper
    
    * Fixed failing tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
    
    * Applied suggestions by Casper
    
    * Added default value to mappingType
    
    * Removed blank line suggested by pre-commit
    
    * Update oteapi/models/mappingconfig.py
    
    Co-authored-by: Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
    
    * Updated MappingConfig
    
    * Renamed mapping type to "triples"
    
    * Fixed failing test
    
    * Added MappingSessionUpdate
    
    * Added RDFTriple
    
    * Cleaned up
    
    Co-authored-by: Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>

--- oteapi/models/mappingconfig.py
+++ oteapi/models/mappingconfig.py
@@ -5,12 +5,15 @@ from pydantic import Field
 
 from oteapi.models.genericconfig import GenericConfig
 
+RDFTriple = Tuple[str, str, str]
+
 
 class MappingConfig(GenericConfig):
     """Mapping Strategy Data Configuration."""
 
     mappingType: str = Field(
-        ..., description="Type of registered mapping strategy. E.g., `mapping/demo`."
+        ...,
+        description="Type of registered mapping strategy.",
     )
     prefixes: Optional[Dict[str, str]] = Field(
         None,
@@ -19,7 +22,7 @@ class MappingConfig(GenericConfig):
             "given as local value/IRI-expansion-pairs."
         ),
     )
-    triples: Optional[List[Tuple[str, str, str]]] = Field(
+    triples: Optional[List[RDFTriple]] = Field(
         None,
-        description="List of semantic triples given as (subject, predicate, object).",
+        description="List of RDF triples given as (subject, predicate, object).",
     )
