bfe3f72a4e185f5266f5445f5c48baeea7dbc6d5
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2022-03-15 10:51:31 +0100

    fix pylint issues

--- oteapi/triplestore/triplestore.py
+++ oteapi/triplestore/triplestore.py
@@ -14,10 +14,12 @@ Features:
 
 import json
 from typing import TYPE_CHECKING
+
+from franz.openrdf.connect import ag_connect
+from franz.openrdf.rio.tupleformat import TupleFormat
 from franz.openrdf.sail.allegrographserver import AllegroGraphServer  # type: ignore
 from franz.openrdf.sail.spec import reason  # type: ignore
-from franz.openrdf.rio.tupleformat import TupleFormat
-from franz.openrdf.connect import ag_connect
+
 from oteapi.models import AttrDict, TripleStoreConfig
 
 if TYPE_CHECKING:  # pragma: no cover
@@ -45,9 +47,7 @@ class TripleStore:
         self,
         config: "Union[TripleStoreConfig, Dict[str, Any]]",
     ) -> None:
-        if config is None:
-            self.config = TripleStoreConfig()
-        elif isinstance(config, (dict, AttrDict)):
+        if isinstance(config, (dict, AttrDict)):
             self.config = TripleStoreConfig(**config)
         elif isinstance(config, TripleStoreConfig):
             self.config = config
@@ -56,76 +56,80 @@ class TripleStore:
                 "config should be either a `TripleStoreConfig` data model or a "
                 "dictionary."
             )
-        self.server = AllegroGraphServer(self.config.agraphHost,
-                                         self.config.agraphPort,
-                                         self.config.agraphUser,
-                                         self.config.agraphPassword)
+        self.server = AllegroGraphServer(
+            self.config.agraphHost,
+            self.config.agraphPort,
+            self.config.agraphUser,
+            self.config.agraphPassword,
+        )
 
-    def add(
-        self,
-        triples: "Any"
-    ) -> str:
+    def add(self, triples: "Any") -> str:
         """
         Add triples to the triplestore.
 
         Args:
-            triples: triples turtle format(<s> <o> <p>.).           
+            triples: triples turtle format(<s> <o> <p>.).
 
         """
-        connection = ag_connect(self.config.repositoryName,
-                                host=self.config.agraphHost,
-                                port=self.config.agraphPort,
-                                user=self.config.agraphUser,
-                                password=self.config.agraphPassword)
+        connection = ag_connect(
+            self.config.repositoryName,
+            host=self.config.agraphHost,
+            port=self.config.agraphPort,
+            user=self.config.agraphUser,
+            password=self.config.agraphPassword,
+        )
         connection.addData(triples)
         connection.close()
         return "Triples successfully added"
 
-    def get(self, sparqlQuery: str) -> "Any":
+    def get(self, sparql_query: str) -> "Any":
         """Return the query result.
 
         Args:
-            sparqlQuery: The SPARQL search query.
+            sparql_query: The SPARQL search query.
 
         Returns:
             The output of the search query in the form of subject, object and predicate.
 
         """
         connection = self.server.openSession(
-            reason("<" + str(self.config.repositoryName) + ">"))
-        TupleQuery = connection.prepareTupleQuery(query=sparqlQuery)
+            reason("<" + str(self.config.repositoryName) + ">")
+        )
+        tuple_query = connection.prepareTupleQuery(query=sparql_query)
         response = []
-        results = TupleQuery.evaluate(output_format=TupleFormat.JSON)
-        with TupleQuery.evaluate(output_format=TupleFormat.JSON) as results:
+        results = tuple_query.evaluate(output_format=TupleFormat.JSON)
+        with tuple_query.evaluate(output_format=TupleFormat.JSON) as results:
             for result in results:
-                tripleSet = {}
-                if ("'s': " in str(result)):
-                    tripleSet['s'] = str(result.getValue("s"))
-                if ("'p': " in str(result)):
-                    tripleSet['p'] = str(result.getValue("p"))
-                if ("'o': " in str(result)):
-                    tripleSet['o'] = str(result.getValue("o"))
-                response.append((tripleSet))
+                triple_set = {}
+                if "'s': " in str(result):
+                    triple_set["s"] = str(result.getValue("s"))
+                if "'p': " in str(result):
+                    triple_set["p"] = str(result.getValue("p"))
+                if "'o': " in str(result):
+                    triple_set["o"] = str(result.getValue("o"))
+                response.append((triple_set))
         connection.close()
         return json.dumps(response)
 
-    def updateDelete(self, sparqlQuery: str) -> None:
+    def update_delete(self, sparql_query: str) -> None:
         """Remove and update triples.
 
         Useful for modifying and cleaning up mappings.
 
         Args:
-            sparqlQuery: The sparql update/delete query.
+            sparql_query: The sparql update/delete query.
         Returns:
             True if update was successful.
 
         """
-        connection = ag_connect(self.config.repositoryName,
-                                host=self.config.agraphHost,
-                                port=self.config.agraphPort,
-                                user=self.config.agraphUser,
-                                password=self.config.agraphPassword)
-        updateQuery = connection.prepareUpdate(query=sparqlQuery)
-        response = updateQuery.evaluate()
+        connection = ag_connect(
+            self.config.repositoryName,
+            host=self.config.agraphHost,
+            port=self.config.agraphPort,
+            user=self.config.agraphUser,
+            password=self.config.agraphPassword,
+        )
+        update_query = connection.prepareUpdate(query=sparql_query)
+        response = update_query.evaluate()
         connection.close()
         return response
