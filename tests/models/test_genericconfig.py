747e7806ca4e84ff6d864dba9b897ba91c8d8d8e
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-03-30 18:38:26 +0200

    Extend testing to cover more lines

--- tests/models/test_genericconfig.py
+++ tests/models/test_genericconfig.py
@@ -1,15 +1,25 @@
 """Tests for `oteapi.models.genericconfig`"""
-# pylint: disable=no-member
+# pylint: disable=no-member,pointless-statement,disallowed-name
 from typing import TYPE_CHECKING
 
 import pytest
 
 if TYPE_CHECKING:
-    from oteapi.models.genericconfig import GenericConfig
+    from oteapi.models.genericconfig import AttrDict, GenericConfig
+
+    class CustomConfiguration(AttrDict):
+        """A custom AttrDict class to use as `configuration` in CustomConfig."""
+
+        string: str
+
+    class CustomConfig(GenericConfig):
+        """A CustomConfig class."""
+
+        configuration: CustomConfiguration
 
 
 @pytest.fixture
-def generic_config() -> "GenericConfig":
+def generic_config() -> "CustomConfig":
     """Return a usable `GenericConfig` for test purposes."""
     from pydantic import Field
 
@@ -52,12 +62,20 @@ def test_subclass_description() -> None:
     assert instance.description == instance.__doc__
 
 
-def test_attribute_get_item(generic_config: "GenericConfig") -> None:
+def test_attribute_get_item(generic_config: "CustomConfig") -> None:
     """Test configuration.__getitem__."""
     assert generic_config.configuration["integer"] == 5
 
 
-def test_attribute_set(generic_config: "GenericConfig") -> None:
+def test_attribute_get_item_fail(generic_config: "CustomConfig") -> None:
+    """Ensure KeyError is raised for a non-existent key."""
+    non_existent_key = "non_existent_key"
+    assert non_existent_key not in generic_config.configuration
+    with pytest.raises(KeyError):
+        generic_config.configuration[non_existent_key]
+
+
+def test_attribute_set(generic_config: "CustomConfig") -> None:
     """Test configuration.__setitem__, and thus also configuration.__setattr__.
 
     Assign different values to test dynamic type-casting.
@@ -75,15 +93,60 @@ def test_attribute_set(generic_config: "GenericConfig") -> None:
     assert generic_config.configuration["float"] == "0"
 
 
-def test_attribute_contains(generic_config: "GenericConfig") -> None:
+def test_attribute_contains(generic_config: "CustomConfig") -> None:
     """Test confguration.__contains__."""
     assert "float" in generic_config.configuration
 
 
-def test_attribute_del_item(generic_config: "GenericConfig") -> None:
+def test_attribute_del_item(generic_config: "CustomConfig") -> None:
     """Test configuration.__delitem__."""
     del generic_config.configuration["float"]
     assert "float" not in generic_config.configuration
+
+    # "string" is defined as a pydantic model field.
+    # This means it should not be deleted, but reset to its default value
+    # and removed from the set of set fields.
+    generic_config.configuration["string"] = "my secret string"
+    assert generic_config.configuration.string == "my secret string"
+    assert "string" in generic_config.configuration.__fields_set__
+
+    del generic_config.configuration["string"]
+    assert "string" in generic_config.configuration
+    assert (
+        generic_config.configuration.string
+        == generic_config.configuration.__fields__["string"].default
+    )
+    assert "string" not in generic_config.configuration.__fields_set__
+
+
+def test_attribute_del_item_fail(generic_config: "CustomConfig") -> None:
+    """Ensure KeyError is raised if key does not exist in AttrDict."""
+    non_existent_key = "non_existant_key"
+    assert non_existent_key not in generic_config.configuration
+    with pytest.raises(KeyError):
+        del generic_config.configuration[non_existent_key]
+
+    # Make sure deleting a pydantic field does not fully delete it
+    # and hence does not raise KeyError.
+    del generic_config.configuration["string"]
+    del generic_config.configuration["string"]
+
+
+def test_attribute_ne(generic_config: "CustomConfig") -> None:
+    """Test configuration.__ne__()."""
+    from pydantic import BaseModel
+
+    class Foo(BaseModel):
+        """Foo pydantic model."""
+
+        foo: str
+
+    assert generic_config.configuration != Foo(foo="foo")
+    assert generic_config.configuration != {"foo": "foo"}
+    assert generic_config.configuration != 2
+
+    copy_config = generic_config.configuration.copy(deep=True)
+    assert (generic_config.configuration != copy_config) is False
 
 
 def test_attrdict() -> None:
