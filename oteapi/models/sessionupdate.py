090209f702e437dc4d88e446cbb5eb090c51fb99
Thomas F. Hagelien <thomas.f.hagelien@sintef.no>
2022-08-17 11:56:08 +0200

    Updated sessionupdate datamodel to include session_type and session_id

--- oteapi/models/sessionupdate.py
+++ oteapi/models/sessionupdate.py
@@ -1,6 +1,41 @@
-"""Pydantic SessionUpdate Data Model."""
-from oteapi.models.genericconfig import AttrDict
+"""Pydantic SessionUpdate Data Model.
 
+Note that the Session Object should be updated according to the
+SessionUpdate. If the Session already has defined a session_type
+and session_id, the code should throw an exception.
+"""
 
-class SessionUpdate(AttrDict):
-    """Session Update Data Model for returning values."""
+from typing import Optional
+from pydantic import ValidationError, root_validator, validator, Field
+from oteapi.models import AttrDict
+
+class OTESession(AttrDict):
+    """ Session Update Data Model for returning data from strategies. """
+    session_type: Optional[str] = Field(
+        None,
+        description="Describe the current session type"
+
+    )
+    session_id : Optional[str] = Field(
+        None,
+        description="Optional ID for referring to session data"
+    )
+
+    @root_validator(pre=True)
+    def check_session_assignment(cls, values):
+        """ check that session_type is defined if session_id is given """
+        session_id, session_type = values.get('session_id'), values.get('session_type')
+        if session_id is not None and session_type is None:
+            raise ValueError('session_id cannot be defined without specifying session_type')
+        return values
+
+    @validator('session_id', pre=True)
+    def check_session_type_exist(cls, v, values):
+        """ check that session_type is defined if session_id set """
+        if v is not None and 'session_type' in values:
+            raise ValueError('session_id cannot be defined without specifying session_type')
+        return v
+
+    class Config:
+        """ enable assignment validation """
+        validate_assignment = True
