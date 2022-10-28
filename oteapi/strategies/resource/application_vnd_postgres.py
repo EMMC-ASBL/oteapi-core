f9dbcbea3357582d5562061d46c4994a63269893
Daniel Marchand <daniel.marchand@sintef.no>
2022-10-28 17:16:48 +0200

    working proof of concept postgrep resource strategy

--- oteapi/strategies/resource/application_vnd_postgres.py
+++ oteapi/strategies/resource/application_vnd_postgres.py
@@ -9,38 +9,45 @@ from pydantic.dataclasses import dataclass
 
 from oteapi.datacache import DataCache
 from oteapi.models import AttrDict, DataCacheConfig, ResourceConfig, SessionUpdate
+from oteapi.models.resourceconfig import HostlessAnyUrl
 from oteapi.plugins import create_strategy
 
 if TYPE_CHECKING:  # pragma: no cover
     from typing import Any, Dict
 
 
-# NOTE: common to all SQL configs
-class PostgresParseConfig(AttrDict):
+class PostgresConnectionConfig(AttrDict):
     """Configuration data model for
-    [`PostgresParseStrategy`][oteapi.strategies.parse.application_vnd_postgres.PostgresParseStrategy]."""
+    [`PostgresResourceStrategy`][oteapi.strategies.resource.application_vnd_postgres.PostgresParseStrategy]."""
 
+    # TODO this is *NOT* where the sqlquery should be placed! 
     sqlquery: str = Field("", description="A SQL query string.")
+    # TODO not the final way to pass connection info (should be a dictionary-like object)
+    connection_str: str = Field("", description="String used for connection with postgres DB via psycopg.")
     datacache_config: Optional[DataCacheConfig] = Field(
         None,
         description="Configuration options for the local data cache.",
     )
 
 
-class PostgresParserResourceConfig(ResourceConfig):
-    """Postgres parse strategy resource config."""
+# 
+class PostgresResourceConfig(ResourceConfig):
+    """Postgres resource strategy resource config."""
 
-    mediaType: str = Field(
-        "application/vnd.postgres",
-        const=True,
-        description=ResourceConfig.__fields__["mediaType"].field_info.description,
+    accessUrl: HostlessAnyUrl = Field(
+        ...,
+        description=ResourceConfig.__fields__["accessUrl"].field_info.description,
     )
-    configuration: PostgresParseConfig = Field(
-        PostgresParseConfig(), description="Postgres parse strategy-specific configuration."
+    accessService: Optional[str] = Field(
+        str, description="services to use for access?"
+    )
+    configuration: PostgresConnectionConfig = Field(
+        PostgresConnectionConfig(), description="Postgres resource strategy-specific configuration."
     )
 
 
-def create_connection(conn_config: dict) -> psycopg.Connection:
+# TODO: using a string for conn_config to demo, but should go back to dictionary later
+def create_connection(conn_config: str) -> psycopg.Connection:
     """Create a database connection to Postgres database.
 
     Parameters:
@@ -54,20 +61,20 @@ def create_connection(conn_config: dict) -> psycopg.Connection:
 
     """
     try:
-        return psycopg.connect(**conn_config)
+        return psycopg.connect(conn_config)
     except psycopg.Error as exc:
         raise psycopg.Error("Could not connect to given Postgres DB.") from exc
 
 
-class SessionUpdatePostgresParse(SessionUpdate):
-    """Configuration model for PostgresParse."""
+class SessionUpdatePostgresResource(SessionUpdate):
+    """Configuration model for PostgresResource."""
 
     result: list = Field(..., description="List of results from the query.")
 
 
 @dataclass
-class PostgresParseStrategy:
-    """Parse strategy for Postgres.
+class PostgresResourceStrategy:
+    """Resource strategy for Postgres.
 
     **Registers strategies**:
 
@@ -78,7 +85,7 @@ class PostgresParseStrategy:
 
     """
 
-    parse_config: PostgresParserResourceConfig
+    resource_config: PostgresResourceConfig 
 
     def initialize(self, session: "Optional[Dict[str, Any]]" = None) -> SessionUpdate:
         """Initialize strategy."""
@@ -86,23 +93,23 @@ class PostgresParseStrategy:
 
     def get(
         self, session: "Optional[Dict[str, Any]]" = None
-    ) -> SessionUpdatePostgresParse:
-        """Parse Postgres query responses."""
+    ) -> SessionUpdatePostgresResource:
+        """Resource Postgres query responses."""
         if session:
             self._use_filters(session)
         session = session if session else {}
 
         # Not yet sure how to set up the connection config...
-        conn_config = self.parse_config.copy()['conn_config']
+        conn_config = self.resource_config.configuration.connection_str
 
         connection = create_connection(conn_config)
         cursor = connection.cursor()
-        result = cursor.execute(self.parse_config.configuration.sqlquery).fetchall()
+        result = cursor.execute(self.resource_config.configuration.sqlquery).fetchall()
         connection.close()
-        return SessionUpdatePostgresParse(result=result)
+        return SessionUpdatePostgresResource(result=result)
 
     def _use_filters(self, session: "Dict[str, Any]") -> None:
         """Update `config` according to filter values found in the session."""
-        if "sqlquery" in session and not self.parse_config.configuration.sqlquery:
+        if "sqlquery" in session and not self.resource_config.configuration.sqlquery:
             # Use SQL query available in session
-            self.parse_config.configuration.sqlquery = session["sqlquery"]
+            self.resource_config.configuration.sqlquery = session["sqlquery"]
