659c280b1247155858da45f8661d9ca33ea7ac6e
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2023-09-21 10:06:39 +0200

    Update _pydantic according to ruff and mypy

--- oteapi/utils/_pydantic.py
+++ oteapi/utils/_pydantic.py
@@ -18,10 +18,10 @@ def get_pydantic_major_version() -> int:
 PYDANTIC_VERSION = get_pydantic_major_version()
 
 if PYDANTIC_VERSION == 1:
-    from pydantic import *
-    from pydantic.fields import Undefined
+    from pydantic import *  # noqa: F403
+    from pydantic.fields import Undefined  # noqa: F401
 elif PYDANTIC_VERSION == 2:
-    from pydantic.v1 import *
-    from pydantic.v1.fields import Undefined
+    from pydantic.v1 import *  # noqa: F403
+    from pydantic.v1.fields import Undefined  # type: ignore[no-redef]  # noqa: F401
 else:
     raise NotImplementedError("Support for pydantic version >=3 is not implemented.")
