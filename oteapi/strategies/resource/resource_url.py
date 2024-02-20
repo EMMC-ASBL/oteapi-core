95cabede4cedd1098d65bec3a98ab9924d12f542
TreesaJ <treesarjc@gmail.com>
2024-02-20 11:12:52 +0100

    fix pre-commit

--- oteapi/strategies/resource/resource_url.py
+++ oteapi/strategies/resource/resource_url.py
@@ -5,7 +5,6 @@ from typing import Literal
 from pydantic import AnyHttpUrl, Field
 from pydantic.dataclasses import dataclass
 
-from oteapi.models import AttrDict
 from oteapi.models.resourceconfig import ResourceConfig
 
 
