08e066836d83c8d9c231a59bd7b93b253eccb5d6
Jesper Friis <jesper.friis@sintef.no>
2024-02-29 15:19:38 +0100

    Added example and IRI fields

--- oteapi/models/resourceconfig.py
+++ oteapi/models/resourceconfig.py
@@ -29,9 +29,10 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "distribution is available directly, typically through a HTTPS "
             "GET request or SFTP.\n\n"
             "Range: Resource\n\n"
-            "Example: http://dcat.example.org/files/001.csv\n\n"
-            "Reference: cdat:downloadURL"
+            "Reference: dcat:downloadURL"
         ),
+        example="http://dcat.example.org/files/001.csv",  # type: ignore
+        IRI="http://www.w3.org/ns/dcat#downloadURL",  # type: ignore
     )
     mediaType: Optional[str] = Field(
         None,
@@ -41,12 +42,13 @@ class ResourceConfig(GenericConfig, SecretConfig):
             ".Usage: This property *SHOULD* be used when the media"
             " type of the distribution is defined in IANA "
             "[[IANA-MEDIA-TYPES](https://www.w3.org/TR/vocab-dcat-2/#bib-iana-media-types)].\n\n"
-            "Example: text/csv\n\n"
             "Note: In a data catalog should the full IRI be used, i.e. "
             "`http://www.iana.org/assignments/media-types/` should be "
             "pre-pended to the value in this field."
             "Reference: dcat:mediaType"
         ),
+        example="text/csv",  # type: ignore
+        IRI="http://www.w3.org/ns/dcat#mediaType",  # type: ignore
     )
     accessUrl: Optional[HostlessAnyUrl] = Field(
         None,
@@ -58,9 +60,10 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "typically through a Web form, query or API call.\n"
             "`downloadURL` is preferred for direct links to downloadable "
             "resources.\n\n"
-            "Example: http://dcat.example.org/dataset-002.html\n\n"
             "Reference: dcat:accessURL"
         ),
+        example="http://dcat.example.org/dataset-002.html",  # type: ignore
+        IRI="http://www.w3.org/ns/dcat#accessURL",  # type: ignore
     )
     accessService: Optional[str] = Field(
         None,
@@ -70,9 +73,10 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "Usage: SHOULD be used to link to a description of a "
             "dcat:DataService that can provide access to this distribution.\n\n"
             "Range: Resource\n\n"
-            "Example: http://dcat.example.org/table-service-005\n\n"
             "Reference: dcat:accessService"
         ),
+        example="http://dcat.example.org/table-service-005",  # type: ignore
+        IRI="http://www.w3.org/ns/dcat#accessService",  # type: ignore
     )
     license: Optional[str] = Field(
         None,
@@ -83,9 +87,10 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "for the Resource. See also guidance at [9. License and rights "
             "statements](https://www.w3.org/TR/vocab-dcat-3/#license-rights).\n\n"
             "Range: URL to license document (dcterms:LicenseDocument).\n\n"
-            "Example: https://creativecommons.org/licenses/by/4.0/\n\n"
             "Reference: dcterms:license"
         ),
+        example="https://creativecommons.org/licenses/by/4.0/",  # type: ignore
+        IRI="http://purl.org/dc/terms/license",  # type: ignore
     )
     accessRights: Optional[str] = Field(
         None,
@@ -97,9 +102,10 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "statements](https://www.w3.org/TR/vocab-dcat-3/#license-rights).\n\n"
             "Range: URL to document describing the license rights "
             "(dcterm:RightsStatement).\n\n"
-            "Example: http://publications.europa.eu/resource/authority/access-right/PUBLIC\n\n"
             "Reference: dcterms:accessRights"
         ),
+        example="http://publications.europa.eu/resource/authority/access-right/PUBLIC",  # type: ignore
+        IRI="http://www.w3.org/ns/dcat#accessService",  # type: ignore
     )
     publisher: Optional[str] = Field(
         None,
@@ -109,9 +115,10 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "Usage: Resources of type "
             "[foaf:Agent](http://xmlns.com/foaf/0.1/Agent) are recommended "
             "as values for this property.\n\n"
-            "Example: http://emmc.eu/\n\n"
             "Reference: dcterms:publisher"
         ),
+        example="http://emmc.eu/",  # type: ignore
+        IRI="http://purl.org/dc/terms/publisher",  # type: ignore
     )
     creator: Optional[str] = Field(
         None,
@@ -124,10 +131,13 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "Example: https://orcid.org/0000-0002-1560-809X\n\n"
             "Reference: dcterms:creator"
         ),
+        example="https://orcid.org/0000-0002-1560-809X",  # type: ignore
+        IRI="http://purl.org/dc/terms/creator",  # type: ignore
     )
     title: Optional[str] = Field(
         None,
         description="A name given to the resource.\n\nReference: dcterms:title",
+        IRI="http://purl.org/dc/terms/title",  # type: ignore
     )
     #
     # For now `description` is commented out to avoid name conflice
@@ -140,6 +150,7 @@ class ResourceConfig(GenericConfig, SecretConfig):
     #         "A free-text account of the resource.\n\n"
     #         "Reference: dcterms:description"
     #     ),
+    #     IRI="http://purl.org/dc/terms/description",  # type: ignore
     # )
     keyword: Optional[List[str]] = Field(
         None,
@@ -149,6 +160,7 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "searchable.\n\n"
             "Reference: dcat:keyword"
         ),
+        IRI="http://www.w3.org/ns/dcat#keyword",  # type: ignore
     )
     conformsTo: Optional[str] = Field(
         None,
@@ -160,9 +172,10 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "This is (generally) a complementary concern to the media-type "
             "or format.\n\n"
             "Range: URL to the standard (dcterms:Standard).\n\n"
-            "Example: https://www.w3.org/TR/owl2-overview/\n\n"
             "Reference: dcterms:conformsTo"
         ),
+        example="https://www.w3.org/TR/owl2-overview/",  # type: ignore
+        IRI="http://purl.org/dc/terms/conformsTo",  # type: ignore
     )
     wasGeneratedBy: Optional[str] = Field(
         None,
@@ -171,9 +184,10 @@ class ResourceConfig(GenericConfig, SecretConfig):
             "an activity. This entity did not exist before generation and "
             "becomes available for usage after this generation.\n\n"
             "Range: URL referring generating activity (prov:Activity)\n\n"
-            "Example: http://www.ntnu.edu/temgemini/transmission-electron-microscopy\n\n"
             "Reference: prov:wasGeneratedBy"
         ),
+        example="http://www.ntnu.edu/temgemini/transmission-electron-microscopy",  # type: ignore
+        IRI="http://www.w3.org/ns/prov#wasGeneratedBy",  # type: ignore
     )
 
     @model_validator(mode="after")
