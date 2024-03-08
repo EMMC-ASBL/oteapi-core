e935e35352034ad1223d57703298978153fc162a
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2024-03-08 07:53:51 +0100

    Update resource/url strategy (#440)
    Add mediaType as a requirement and have a test for it.

---
+++ tests/strategies/resource/test_resource_url.py
@@ -0,0 +1,19 @@
+"""Test the resource strategy for resource/url."""
+
+
+def test_resource_url() -> None:
+    """Test `resource/url` resource strategy."""
+    from oteapi.models.genericconfig import AttrDict
+    from oteapi.strategies.resource.resource_url import ResourceURLStrategy
+
+    config = {
+        "resourceType": "resource/url",
+        "downloadUrl": "https://example.org/sample.json",
+        "mediaType": "application/json",
+    }
+    strategy = ResourceURLStrategy(resource_config=config)
+
+    assert strategy.initialize() == AttrDict()
+    assert strategy.get() == AttrDict(
+        downloadUrl="https://example.org/sample.json", mediaType="application/json"
+    )
